d4948016ef18a76be4bc6bab03cb02c2c5beaf5b
idarwin <ian>
2008-11-14 17:19:34 +0000

    Create an Address object for use in Person.

---
+++ src/domain/Address.java
@@ -0,0 +1,41 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private int id;
+	
+	private String streetAddress;
+	private String city;
+	private Country country;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Country getCountry() {
+		return country;
+	}
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+}
