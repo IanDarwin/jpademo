9234f6ecbdc595e4d6c23dfd15940eafb9e63173
idarwin <ian>
2008-11-14 02:53:31 +0000

    Constructors and accessors.

--- src/domain/Person.java
+++ src/domain/Person.java
@@ -11,4 +11,34 @@ public class Person {
 	@Id int id;
 	String firstName;
 	String lastName;
+	
+	public Person() {
+		// empty
+	}
+	
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
