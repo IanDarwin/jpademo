00c6a92aeca01e190e3bb1ce981d4a3d16d833e3
Ian Darwin <ian@darwinsys.com>
2014-02-20 13:55:22 -0500

    Imports police.

--- src/jpa/features/CriteriaQueryExample.java
+++ src/jpa/features/CriteriaQueryExample.java
@@ -1,18 +1,14 @@
 package jpa.features;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.ParameterExpression;
-import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import jpa.JPAUtil;
-
 import domain.Person;
 
 public class CriteriaQueryExample {
