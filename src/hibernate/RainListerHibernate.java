b164344324865956367d7b425f0b64d2d14b079a
Ian Darwin <ian@darwinsys.com>
2014-06-20 10:16:46 -0400

    Make RainLister programs closer to identical.

--- src/hibernate/RainListerHibernate.java
+++ src/hibernate/RainListerHibernate.java
@@ -16,6 +16,15 @@ public class RainListerHibernate {
 
 	public static void main(String[] args) throws HibernateException {
 			final MusicDaoHibernateImpl musicDAO = new MusicDaoHibernateImpl();
+
+			MusicRecording recording1 = new MusicRecording("The Fray", "How to Save a Life", 9.67, "Rock", null);
+			musicDAO.saveMusicRecording(recording1);
+			System.out.printf("Added Music Recording %d.%n", recording1.getId());
+			
+			MusicRecording	recording2 = new MusicRecording("K.T.Tunstall", "Eye to the Telescope", 8.75, "Pop", null);
+			musicDAO.saveMusicRecording(recording2);
+			System.out.printf("Added Music Recording %d.%n", recording2.getId());
+			
 			final List<MusicRecording> list = 
 				musicDAO.findRecordingsByPrice(9.67);
 			
