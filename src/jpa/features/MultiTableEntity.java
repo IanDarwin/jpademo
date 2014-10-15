f8317ec8527f515926ae4346d0ca54928d14f306
Ian Darwin <ian@darwinsys.com>
2014-10-15 16:15:28 -0400

    Self-contained demo of an entity mapped into two tables.
    Suggested by a student question on a GM BootCamp G979 (936-based).

---
+++ src/jpa/features/MultiTableEntity.java
@@ -0,0 +1,72 @@
+package jpa.features;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+
+import jpa.JPAUtil;
+
+@Entity
+@Table(name="MultiTableEntity")
+@SecondaryTable(name="SecondaryTable")
+public class MultiTableEntity {
+
+	private int id;
+	/** These fields go into the main table */
+	private String mainField1, mainfield2;
+	/** These fields go into the secondary table */
+	private String secoField1, secoField2;
+	
+	public static void main(String[] args) {
+		EntityManager em = JPAUtil.getEntityManager();
+		MultiTableEntity entity = new MultiTableEntity();
+		entity.mainField1 = "one";
+		entity.mainfield2 = "two";
+		entity.secoField1 = "three";
+		entity.secoField2 = "four";
+		em.getTransaction().begin();
+		em.persist(entity);
+		em.getTransaction().commit();
+		em.close();
+		System.out.println("Created entity " + entity.getId());
+	}
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMainField1() {
+		return mainField1;
+	}
+	public void setMainField1(String mainField1) {
+		this.mainField1 = mainField1;
+	}
+	public String getMainfield2() {
+		return mainfield2;
+	}
+	public void setMainfield2(String mainfield2) {
+		this.mainfield2 = mainfield2;
+	}
+	@Column(table="SecondaryTable")
+	public String getSecoField1() {
+		return secoField1;
+	}
+	public void setSecoField1(String secoField1) {
+		this.secoField1 = secoField1;
+	}
+	@Column(table="SecondaryTable")
+	public String getSecoField2() {
+		return secoField2;
+	}
+	public void setSecoField2(String secoField2) {
+		this.secoField2 = secoField2;
+	}	
+}
