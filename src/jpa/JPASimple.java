984fab0950db13620c2b367604f9cfe9476d55a2
Ian Darwin <ian@darwinsys.com>
2015-01-12 11:18:22 -0500

    Imports police

--- src/jpa/JPASimple.java
+++ src/jpa/JPASimple.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import domain.Address;
