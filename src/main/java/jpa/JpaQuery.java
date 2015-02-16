69df9aa4182bf551754335e954c13c5b30ddee58
Ian Darwin <ian@darwinsys.com>
2015-02-16 11:23:50 -0500

    Bring back JpaQuery interactive runner
    Started life here, moved to darwinsys-api, but only
    useful with a Persistence.xml file, so bring it back
    here.

--- src/main/java/jpa/JpaQuery.java
+++ src/main/java/jpa/JpaQuery.java
@@ -1,2 +1,109 @@
-// This class has been moved to the darwinsys-api project
-// to make it easier to use in other projects.
+package jpa;
+
+import java.io.Console;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.swing.JOptionPane;
+
+/**
+ * Interactive query runner; ideal for testing query syntax once
+ * you've dealt with setting JPA up for your project.
+ */
+public class JpaQuery {
+
+	static EntityManagerFactory entityMgrFactory = null;
+
+	public enum Mode {
+		CONSOLE,
+		SWING
+	}
+
+	public static void main(String[] args) {
+
+		String unitName = args.length == 0 ?  "jpademo" : args[0];
+
+		JpaQuery program = new JpaQuery();
+
+		// Do i/o initializations before JPA to fail fast
+		program.init();
+
+		System.out.println("JPAQuery.main() -- starting JPA " + unitName);
+
+		// This would be done for you
+		// were you running in an EE App Server
+		EntityManager entityManager = null;
+		try {
+			entityMgrFactory = 
+			Persistence.createEntityManagerFactory(unitName);
+			System.out.println("Created EntityManagerFactory");
+			entityManager = entityMgrFactory.createEntityManager();
+
+			// Delegate
+			program.run(entityManager);
+
+		} catch (Exception e) {
+			System.out.println("JPA failure: " + e);
+			e.printStackTrace();
+			return;
+		} finally {	
+			if (entityManager != null)
+				entityManager.close();
+			if (entityMgrFactory != null)
+				entityMgrFactory.close();
+		}
+	}
+		
+	Console console;
+	Mode mode = Mode.CONSOLE;
+
+	void init() {
+        if ((console = System.console()) == null) {
+			mode = Mode.SWING;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	void run(EntityManager entityManager) throws Exception {
+
+		String queryStr;
+		Query query = null;
+
+
+		while(true) {
+
+			try {
+				queryStr = mode == Mode.CONSOLE ? 
+					console.readLine("Enter JPA Query: ") :
+					JOptionPane.showInputDialog(null, "Enter JPA Query: ");
+				if (queryStr == null || "quit".equals(queryStr)) {
+					return;
+				}
+	
+				query = entityManager.createQuery(queryStr);
+	
+				List<Object> list = query.getResultList();
+				System.out.println("Found " + list.size() + " results:");
+				for (Object o : list) {
+					System.out.println(o);
+				}
+				System.out.println();
+			} catch (Exception e) {
+				switch (mode) {
+				case CONSOLE:
+					System.out.println("Error: " + e);
+					e.printStackTrace();
+					break;
+				case SWING:
+					String message = e.toString();
+					JOptionPane.showMessageDialog(null, message);
+					break;
+				}
+			}
+		}
+
+	}
+}
