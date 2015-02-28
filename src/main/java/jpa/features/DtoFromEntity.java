f3d4b330bd87b5c9c41b3e708f5ed152d0eac237
Ian Darwin <ian@darwinsys.com>
2015-02-27 21:55:29 -0500

    Use existing @Entity as though it were a DTO.
    Great idea, suggested by Benoit Rouleau, thx.
    Why dinna I think o' tha?

---
+++ src/main/java/jpa/features/DtoFromEntity.java
@@ -0,0 +1,44 @@
+package jpa.features;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import jpa.JpaUtil;
+import domain.Person;
+import domain.model.VideoRecording;
+
+/** 
+ * Try to use a real @Entity as a DTO instead of
+ * making an explicit DTO class.
+ * @author Ian Darwin
+ */
+public class DtoFromEntity {
+	static String query =
+		// Note that the DTO created with NEW here is not a JPA Entity!
+		// And must be fully qualified since JPA has no "import"
+		"select id, ptype, firstname, lastname from Person";
+	String name;
+	int amount;
+	
+	public static void main(String[] args) {
+		EntityManager em = JpaUtil.getEntityManager();
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Person p = new Person("Greashy", "Smith");
+		em.persist(p);
+		em.flush();
+		
+		Query q = em.createNativeQuery(query, Person.class);
+		final List<Person> resultList = q.getResultList();
+		System.out.println("Person:");
+		for (Person per : resultList) {
+			System.out.println(per);
+		}
+		tx.commit();
+	}
+}
