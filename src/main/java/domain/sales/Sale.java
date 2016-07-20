6150ac9ae7bcca84638538edd3738a8ded323755
Ian <ian@darwinsys.com>
2016-07-20 12:29:38 -0400

    Placeholder for Sale entity

---
+++ src/main/java/domain/sales/Sale.java
@@ -0,0 +1,12 @@
+package domain.sales;
+
+/**
+ * Sale entity - track one sale.
+ */
+public class Sale {
+	private long id;
+	private Customer cust;
+	private SalesPerson salesRep
+	private Date salesDate;
+	double amount;
+}
