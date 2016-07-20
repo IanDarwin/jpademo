7c38ebe8bcde9d89be29a9aa71eeb5bb64d9d08c
Ian <ian@darwinsys.com>
2016-07-20 12:24:30 -0400

    Remove unused

--- src/main/java/jpa/features/SalesReportDTO.java
+++ src/main/java/jpa/features/SalesReportDTO.java
@@ -8,20 +8,11 @@ package jpa.features;
 public class SalesReportDTO {
 	private String name;
 	private int amount;
-	private long id;
 	
 	public SalesReportDTO(String name, int amount) {
 		super();
 		this.name = name;
 		this.amount = amount;
-	}
-	
-	public long getId() {
-		return this.id;
-	}
-	@SuppressWarnings("unused") // For JPA
-	private void setId(long id) {
-		this.id = id;
 	}
 	
 	public int getAmount() {
@@ -30,6 +21,7 @@ public class SalesReportDTO {
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+	
 	public String getName() {
 		return name;
 	}
