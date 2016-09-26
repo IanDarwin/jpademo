0f04751803eb69a025c7daba3a2f00cf58990a8e
Ian <ian@darwinsys.com>
2016-09-26 12:15:13 -0400

    Clean up, make it compile, may or may not work.

--- src/main/java/jpa/features/JpaJdbc.java
+++ src/main/java/jpa/features/JpaJdbc.java
@@ -6,10 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.persistence.EntityManager;
-
-import jpa.JpaUtil;
-
-import org.hibernate.Session;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 
 import domain.misc.Fish;
 import domain.misc.FishPK;
@@ -17,10 +16,13 @@ import domain.misc.FishPK;
 /**
  * Demonstrates using the Hibernate Session to perform "raw" JDBC ops
  * but will only work if Hibernate is the underlying JPA provider.
+ * The notion of mixing JDBC with JPA is a *really bad* idea; you are probably
+ * better off using the NativeQuery mechanism if possible.
  */
 public class JpaJdbc {
 	public static void main(String[] args) throws SQLException {
-		EntityManager em = JpaUtil.getEntityManager();
+		EntityManagerFactory entityMgrFactory = Persistence.createEntityManagerFactory("jpademo");
+		EntityManager em = entityMgrFactory.createEntityManager();
 		Fish goldie = new Fish();
 		goldie.setFishName("Goldie");
 		FishPK pk = new FishPK(100, 100);
@@ -30,15 +32,18 @@ public class JpaJdbc {
 		em.getTransaction().commit();
 		em.close();
 		
-		em = JpaUtil.getEntityManager();
+		em = entityMgrFactory.createEntityManager();
 		System.out.println("Using EM: " + em.getClass().getName());
 		Connection conn = null;
 		try {
-			Class<?> clazz = Session.class;
-			conn = em.unwrap(Session.class).connection();
+			conn = em.unwrap(Connection.class);
 		}  catch (PersistenceException pex) {
-			System.err.println("Sorry, EntityManager " + em + " does not support " + clazz);
+			System.err.println("Sorry, EntityManager " + em + " does not support " + "Connection");
 			System.err.println("Try using Hibernate as your EntityManager next time.");
+			System.exit(1);
+		}
+		if (conn == null) {
+			System.err.println("EM gave us a NULL JDBC Connection, sorry!");
 			System.exit(1);
 		}
 		PreparedStatement st = conn.prepareStatement("select * from fish");
