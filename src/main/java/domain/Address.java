47a5b8d6864bf1f3d0f2e38250010d9710a7ba70
Ian Darwin <ian@darwinsys.com>
2018-01-16 13:09:54 -0500

    Favor javax.validation annotations over Hibernate

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -4,17 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Address {
 	
 	private int id;
 	
-	@NotEmpty
+	@NotNull @Size(min=1)
 	private String streetAddress;
-	@NotEmpty
+	@NotNull @Size(min=1)
 	private String city;
 	private Country country = Country.CANADA;
 
