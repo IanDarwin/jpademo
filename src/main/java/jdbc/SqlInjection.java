9a74465f4fdafa3d159708d17334af4fc2c47fde
Ian Darwin <ian@darwinsys.com>
2021-02-04 10:21:15 -0500

    Flesh out to make runnable demo

--- src/main/java/jdbc/SqlInjection.java
+++ src/main/java/jdbc/SqlInjection.java
@@ -4,13 +4,24 @@ package jdbc;
  * Demo of SQL Injection Attacks
  */
 public class SqlInjection {
-	public void breakable(String name) {
-		String sql = "select * from students where name = '" + name + "';";
-		executeQuery(sql);
+	public static void main(String[] args) {
+		String name = readNameFromUser();
+		getStudent(name);
 	}
 
-	private void executeQuery(String sql) {
+	public static void getStudent(String name) {
+		String sql = "select * from students where name = '" + name + "';";
+		String student = executeQuery(sql);
+	}
+
+	private static String executeQuery(String sql) {
+		System.out.println("Executing query: " + sql);
 		// This method would send the query string straight into the database
 		// This is a bad thing...
+		return null;
+	}
+
+	private static String readNameFromUser() {
+		return "Robert'; drop table students; --";	// XKCD Exploits of a Mom
 	}
 }
