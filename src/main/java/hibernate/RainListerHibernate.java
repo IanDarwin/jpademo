152f24ef944415b8d16be3b8de05c40595c15bb1
Ian Darwin <ian@darwinsys.com>
2022-04-26 20:53:25 -0400

    Further contort to fit new Dao interface. Lost the Hibernate DAO impl, alas.

--- src/main/java/hibernate/RainListerHibernate.java
+++ src/main/java/hibernate/RainListerHibernate.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.hibernate.HibernateException;
 
-import dao.MusicDao;
-import dao.MusicDaoHibernateImpl;
+import dao.MusicDataAccessor;
 import domain.model.MusicRecording;
-import domain.model.Track;
 
 /**
  * Main program for Hibernate Demo
@@ -15,18 +13,18 @@ import domain.model.Track;
 public class RainListerHibernate {
 
 	public static void main(String[] args) throws HibernateException {
-			final MusicDao musicDAO = new MusicDaoHibernateImpl();
+			final MusicDataAccessor musicDAO = new MusicDataAccessor();
 
 			MusicRecording recording1 = new MusicRecording("The Fray", "How to Save a Life", 9.67, "Rock", null);
-			musicDAO.saveMusicRecording(recording1);
+			musicDAO.saveRecording(recording1);
 			System.out.printf("Added Music Recording %d.%n", recording1.getId());
 			
 			MusicRecording	recording2 = new MusicRecording("K.T.Tunstall", "Eye to the Telescope", 8.75, "Pop", null);
-			musicDAO.saveMusicRecording(recording2);
+			musicDAO.saveRecording(recording2);
 			System.out.printf("Added Music Recording %d.%n", recording2.getId());
 			
 			final List<MusicRecording> list = 
-				musicDAO.findRecordingsByPrice(9.67);
+				musicDAO.getRecordings("Jazz");
 			
 			list.forEach(mr -> {
 				System.out.println("Recording: " + mr);
--- src/main/java/jpa/RainListerJpa.java
+++ src/main/java/jpa/RainListerJpa.java
@@ -2,8 +2,8 @@ package jpa;
 
 import java.util.List;
 
-import dao.MusicDao;
-import dao.MusicDaoJpaImpl;
+import dao.RecordingDao;
+import dao.MusicDataAccessor;
 import domain.model.MusicRecording;
 
 /**
@@ -14,18 +14,17 @@ public class RainListerJpa {
 	public static void main(String[] args) {
 
 		System.out.println("JPA Demo");
-		MusicDao musicDAO = new MusicDaoJpaImpl();
+		RecordingDao<MusicRecording> musicDAO = new MusicDataAccessor();
 
 		MusicRecording recording1 = new MusicRecording("The Fray", "How to Save a Life", 9.67, "Rock", null);
-		musicDAO.saveMusicRecording(recording1);
+		musicDAO.saveRecording(recording1);
 		System.out.printf("Added Music Recording %d.%n", recording1.getId());
 		
 		MusicRecording	recording2 = new MusicRecording("K.T.Tunstall", "Eye to the Telescope", 8.75, "Pop", null);
-		musicDAO.saveMusicRecording(recording2);
+		musicDAO.saveRecording(recording2);
 		System.out.printf("Added Music Recording %d.%n", recording2.getId());
 
-		List<MusicRecording> recs = musicDAO.findRecordingsByPrice(9.67);
-		
+		List<MusicRecording> recs = musicDAO.getRecordings("Jazz");
 		recs.forEach(System.out::println);	
 	}
 
